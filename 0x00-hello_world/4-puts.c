842b1d541e61a92c3f8827532b519fe45e88f2a8
Victor Dibia <nnamdyvictor@gmail.com>
2023-02-16 18:08:51 -0800

    first C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  *
  * Return: 0
 */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-Return(0);
+   puts("\"Programming is like building a multilingual puzzle");
+   Return (0);
 }
