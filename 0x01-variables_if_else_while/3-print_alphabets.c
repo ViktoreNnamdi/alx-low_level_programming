501b6430db9384941835e0de52c61082b642800a
Victor Dibia <nnamdyvictor@gmail.com>
2023-02-19 17:04:13 -0800

    print lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print in lower and upper case
+ *
+ * Return: Aways 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z' letter++)
+		 putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z' letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
