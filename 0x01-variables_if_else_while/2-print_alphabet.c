f1bf43c056a75b2ac8f9d89e7f7b2ba39eea6587
Victor Dibia <nnamdyvictor@gmail.com>
2023-02-19 16:29:32 -0800

    alphabets in lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, followed by a new line
+ * main - prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
@@ -12,7 +12,7 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-		putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
