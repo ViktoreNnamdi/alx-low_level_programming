442f8be80cc1efc6343969c5f0b9e6f154709ff6
Victor Dibia <nnamdyvictor@gmail.com>
2023-02-19 17:24:38 -0800

    print lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,12 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		 putchar(letter);
+		putchar(letter);
 
 	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 
-	putchar('\n');
+		putchar('\n');
 
 	return (0);
 }
