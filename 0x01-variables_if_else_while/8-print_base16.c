55dfe36ca48631e556a4c886e5622862bb520d2b
Victor Dibia <nnamdyvictor@gmail.com>
2023-02-20 20:57:43 -0800

    8 task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -20,16 +20,3 @@ int main(void)
 
 	return (0);
 }
-
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
-
-	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
-
-
-		putchar('\n');
-
-		return (0);
-}
